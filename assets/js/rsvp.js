65e220b8e5bfcbf17f593446cf95a42467694ba4
Rich Bradish <43855684@hsbc.com>
2017-01-04 10:57:19 +0000

    commit

--- assets/js/rsvp.js
+++ assets/js/rsvp.js
@@ -26,15 +26,84 @@ console.clear(); // <-- keep the console clean on refresh
     };
     
     rsvp.fields = [
-	{
-		className: 'section-label',
-		template:  '<hr/><div><strong>Your {{name}}</strong></div>',
-		fieldGroup: [{
-			className: 'col-xs-6',
-			type: 'input',
-			key: 'guestOne'
-		}]
-	}
+      {
+        className: 'row',
+        fieldGroup: [
+          {
+            className: 'col-xs-6',
+            type: 'input',
+            key: 'firstName',
+            templateOptions: {
+              label: 'First Name'
+            }
+          },
+          {
+            className: 'col-xs-6',
+            type: 'input',
+            key: 'lastName',
+            templateOptions: {
+              label: 'Last Name'
+            },
+            expressionProperties: {
+              'templateOptions.disabled': '!model.firstName'
+            }
+          }
+        ]
+      },
+      {
+        className: 'section-label',
+        template: '<hr /><div><strong>Address:</strong></div>'
+      },
+      {
+        className: 'row',
+        fieldGroup: [
+          {
+            className: 'col-xs-6',
+            type: 'input',
+            key: 'street',
+            templateOptions: {
+              label: 'Street'
+            }
+          },
+          {
+            className: 'col-xs-3',
+            type: 'input',
+            key: 'cityName',
+            templateOptions: {
+              label: 'City'
+            }
+          },
+          {
+            className: 'col-xs-3',
+            type: 'input',
+            key: 'zip',
+            templateOptions: {
+              type: 'number',
+              label: 'Zip',
+              max: 99999,
+              min: 0,
+              pattern: '\\d{5}'
+            }
+          }
+        ]
+      },
+      {
+        template: '<hr />'
+      },
+      {
+        type: 'input',
+        key: 'otherInput',
+        templateOptions: {
+          label: 'Other Input'
+        }
+      },
+      {
+        type: 'checkbox',
+        key: 'otherToo',
+        templateOptions: {
+          label: 'Other Checkbox'
+        }
+      }
     ];
 
     // function definition
