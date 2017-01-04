fcb9792177c1bd2f3690087166b23e7074f08b5b
Rich Bradish <43855684@hsbc.com>
2017-01-04 10:15:57 +0000

    formly additions

---
+++ assets/js/rsvp.js
@@ -0,0 +1,132 @@
+console.clear(); // <-- keep the console clean on refresh
+
+/* global angular */
+(function() {
+  'use strict';
+
+  var app = angular.module('weddingApp', ['formly', 'formlyBootstrap'], function config(formlyConfigProvider) {
+    // set templates here
+    formlyConfigProvider.setType({
+      name: 'custom',
+      templateUrl: 'custom.html'
+    });
+  });
+  
+
+  app.controller('RSVPCtrl', function RSVPCtrl(formlyVersion) {
+    var rsvp = this;
+    // function assignment
+    rsvp.onSubmit = onSubmit;
+
+    // variable assignment
+    rsvp.author = {
+      name: 'Rich Bradish'
+    };
+
+    rsvp.model = {
+      onFriday = false,
+      onSaturday = false
+    };
+    rsvp.options = {
+    };
+    
+    rsvp.fields = [
+      {
+        key: 'text',
+        type: 'input',
+        templateOptions: {
+          label: 'Text',
+          placeholder: 'Formly is terrific!'
+        }
+      },
+      {
+        key: 'nested.story',
+        type: 'textarea',
+        templateOptions: {
+          label: 'Some sweet story',
+          placeholder: 'It allows you to build and maintain your forms with the ease of JavaScript :-)',
+          description: ''
+        },
+        expressionProperties: {
+          'templateOptions.focus': 'formState.awesomeIsForced',
+          'templateOptions.description': function(viewValue, modelValue, scope) {
+            if (scope.formState.awesomeIsForced) {
+              return 'And look! This field magically got focus!';
+            }
+          }
+        }
+      },
+      {
+        key: 'awesome',
+        type: 'checkbox',
+        templateOptions: { label: '' },
+        expressionProperties: {
+          'templateOptions.disabled': 'formState.awesomeIsForced',
+          'templateOptions.label': function(viewValue, modelValue, scope) {
+            if (scope.formState.awesomeIsForced) {
+              return 'Too bad, formly is really awesome...';
+            } else {
+              return 'Is formly totally awesome? (uncheck this and see what happens)';
+            }
+          }
+        }
+      },
+      {
+        key: 'whyNot',
+        type: 'textarea',
+        expressionProperties: {
+          'templateOptions.placeholder': function(viewValue, modelValue, scope) {
+            if (scope.formState.awesomeIsForced) {
+              return 'Too bad... It really is awesome! Wasn\'t that cool?';
+            } else {
+              return 'Type in here... I dare you';
+            }
+          },
+          'templateOptions.disabled': 'formState.awesomeIsForced'
+        },
+        hideExpression: 'model.awesome',
+        templateOptions: {
+          label: 'Why Not?',
+          placeholder: 'Type in here... I dare you'
+        },
+        watcher: {
+          listener: function(field, newValue, oldValue, formScope, stopWatching) {
+            if (newValue) {
+              stopWatching();
+              formScope.model.awesome = true;
+              formScope.model.whyNot = undefined;
+              field.hideExpression = null;
+              formScope.options.formState.awesomeIsForced = true;
+            }
+          }
+        }
+      },
+      {
+        key: 'custom',
+        type: 'custom',
+        templateOptions: {
+          label: 'Custom inlined',
+        }
+      },
+      {
+        key: 'exampleDirective',
+        template: '<div example-directive></div>',
+        templateOptions: {
+          label: 'Example Directive',
+        }
+      }
+    ];
+
+    // function definition
+    function onSubmit() {
+      alert(JSON.stringify(rsvp.model), null, 2);
+    }
+  });
+
+  
+  app.directive('exampleDirective', function() {
+    return {
+      templateUrl: 'example-directive.html'
+    };
+  });
+})();
